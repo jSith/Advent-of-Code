084770702a28491f1bd052ca65906a3edab57b4a
jSith <30677679+jSith@users.noreply.github.com>
2017-12-17 20:52:18 -0600

    add part 2

--- day4.py
+++ day4.py
@@ -5,7 +5,7 @@ def valid_passphrases(lines):
     valid_count = 0
     for line in lines:
         line = line.split(' ')
-        if is_unique(line):
+        if is_unique(line) and not contains_anagram(line):
             valid_count = valid_count+1
     return valid_count
 
@@ -19,8 +19,32 @@ def is_unique(line):
             return False
     return True
 
+
+def contains_anagram(line):
+    for word in line:
+        for test_word in line:
+            if word != test_word:
+                if is_anagram(word, test_word):
+                    return True
+    return False
+
+
+def is_anagram(test, word):
+    for char in test:
+        if char in word:
+            word = word.replace(char, '', 1)
+        else:
+            return False
+
+    if len(word) == 0:
+        return True
+    else:
+        return False
+
+
 def main():
     print(valid_passphrases(utility.clean_input(utility.read_by_line('input4.txt'), False)))
+
 
 if __name__ == '__main__':
     main()
