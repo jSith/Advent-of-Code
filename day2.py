4d8ae369bc2290d76a90967e2135f7a2d35ce24c
jSith <30677679+jSith@users.noreply.github.com>
2017-12-17 20:37:52 -0600

    adding part 2

--- day2.py
+++ day2.py
@@ -25,9 +25,22 @@ def checksum(lines):
     return checksum
 
 
+def evenly_divisible(lines):
+    total = 0
+    for line in lines:
+        total = total + find_factor(line)
+    return total
+
+
+def find_factor(line):
+    for i in line:
+        for j in line:
+            if i != j and i % j == 0:
+                return int(i / j)
+            
 
 def main():
-    print(checksum(utility.clean_input(utility.read_by_line('input2.txt'))))
+    print(evenly_divisible(utility.clean_input(utility.read_by_line('input2.txt'))))
 
 if __name__ == '__main__':
     main()
