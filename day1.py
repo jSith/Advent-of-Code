1e56feb15b2e39d6c85c757849dd7c673e0a9bdc
jSith <30677679+jSith@users.noreply.github.com>
2017-12-17 20:29:54 -0600

    Part 2 of day1

--- day1.py
+++ day1.py
@@ -1,22 +1,35 @@
 import utility
 
-def sum_duplicates(data):
+
+def sum_duplicates(data, half):
     duplicates = 0
     numbers = utility.convert_to_int(data)
     size = len(numbers)
+    if half:
+        offset = int(size/2)
+    else:
+        offset = 1
 
     for i in range(0, size-1):
-        if numbers[i] == numbers[i+1]:
-            duplicates = duplicates + numbers[i]
+        if (i + offset) > size:
+            index = (i+offset) - size
+        else:
+            index = i + offset
 
-    if numbers[size-1] == numbers[0]:
-        duplicates = duplicates + numbers[0]
+        if index == size:
+            index = index - 1
+
+        if i == size:
+            i = 0
+
+        if numbers[i] == numbers[index]:
+            duplicates = duplicates + numbers[i]
 
     return duplicates
 
 
 def main():
-    print(sum_duplicates(utility.read_input('input1.txt')))
+    print(sum_duplicates(utility.read_input('input1.txt'), True))
 
 if __name__ == '__main__':
     main()
